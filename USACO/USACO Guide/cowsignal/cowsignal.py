15f4efd517f8da43dd25b51567b66017f515ead8
Yuvraj <78926606+yuvraj-del@users.noreply.github.com>
2025-05-26 16:21:06 -0400

    Add files via upload

---
+++ USACO/USACO Guide/cowsignal/cowsignal.py
@@ -0,0 +1,11 @@
+read = open("cowsignal.in")
+open("cowsignal.out", "w")
+m, n, k = map(int, [i for i in read.readline().split()])
+
+world = [[i for i in read.readline() if i != "\n"] for j in range(m)]
+
+for i in range(m*k):
+    for j in range(n*k):
+        print(world[int(i/k)][int(j/k)], end="", file=open("cowsignal.out", "a"))
+    if i != m*k - 1:
+        print("", file=open("cowsignal.out", "a"))
